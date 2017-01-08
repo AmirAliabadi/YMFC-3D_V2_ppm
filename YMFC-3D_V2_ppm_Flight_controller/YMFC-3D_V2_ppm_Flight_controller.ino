d8c1414b704c0ec34e59c101944b3505f221b4f7
AmirAliabadi <amiraliabadi@gmail.com>
2017-01-08 08:30:20 -0800

    Some minor formatting updates.

--- YMFC-3D_V2_ppm_Flight_controller/YMFC-3D_V2_ppm_Flight_controller.ino
+++ YMFC-3D_V2_ppm_Flight_controller/YMFC-3D_V2_ppm_Flight_controller.ino
@@ -196,9 +196,9 @@ void loop(){
   //Let's get the current gyro data and scale it to degrees per second for the pid calculations.
   gyro_signalen();
   
-  gyro_roll_input = (gyro_roll_input * 0.8) + ((gyro_roll / 57.14286) * 0.2);            //Gyro pid input is deg/sec.
+  gyro_roll_input  = (gyro_roll_input * 0.8)  + ((gyro_roll / 57.14286)  * 0.2);         //Gyro pid input is deg/sec.
   gyro_pitch_input = (gyro_pitch_input * 0.8) + ((gyro_pitch / 57.14286) * 0.2);         //Gyro pid input is deg/sec.
-  gyro_yaw_input = (gyro_yaw_input * 0.8) + ((gyro_yaw / 57.14286) * 0.2);               //Gyro pid input is deg/sec.
+  gyro_yaw_input   = (gyro_yaw_input * 0.8)   + ((gyro_yaw / 57.14286)   * 0.2);         //Gyro pid input is deg/sec.
 
   // Serial.print( gyro_roll_input ); Serial.print(" "); Serial.print( gyro_pitch_input ); Serial.print(" "); Serial.println( gyro_yaw_input );  
 
